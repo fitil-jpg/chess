b295495e60aa69b2d16ea64abe18a5ca07c8b758
fitil-jpg <appleproapplemac@gmail.com>
2025-08-19 22:23:39 +0300

    Test bot adjustments

---
+++ tests/test_chess_bot_material_bonus.py
@@ -0,0 +1,19 @@
+import chess
+
+from chess_ai.chess_bot import ChessBot, MATERIAL_DEFICIT_BONUS
+from utils import GameContext
+
+
+def test_chess_bot_material_deficit_capture_bonus():
+    board = chess.Board("8/8/3p4/8/3P4/8/8/4K3 w - - 0 1")
+    bot = ChessBot(chess.WHITE)
+    move = chess.Move.from_uci("d4d5")
+
+    score_even, _ = bot.evaluate_move(
+        board, move, GameContext(material_diff=0, mobility=0, king_safety=0)
+    )
+    score_behind, _ = bot.evaluate_move(
+        board, move, GameContext(material_diff=-2, mobility=0, king_safety=0)
+    )
+
+    assert score_behind - score_even == 2 * MATERIAL_DEFICIT_BONUS
