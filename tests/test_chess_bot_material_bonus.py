7af200af674e6a096325a1d716e583db68375ec6
fitil-jpg <appleproapplemac@gmail.com>
2025-08-20 00:48:28 +0300

    Deterministic move selection and fix tests

--- tests/test_chess_bot_material_bonus.py
+++ tests/test_chess_bot_material_bonus.py
@@ -5,9 +5,11 @@ from utils import GameContext
 
 
 def test_chess_bot_material_deficit_capture_bonus():
-    board = chess.Board("8/8/3p4/8/3P4/8/8/4K3 w - - 0 1")
+    # White pawn on d4 captures a pawn on e5.  When behind in material the
+    # bot should receive an additional bonus for such capturing moves.
+    board = chess.Board("8/8/8/4p3/3P4/8/8/4K3 w - - 0 1")
     bot = ChessBot(chess.WHITE)
-    move = chess.Move.from_uci("d4d5")
+    move = chess.Move.from_uci("d4e5")
 
     score_even, _ = bot.evaluate_move(
         board, move, GameContext(material_diff=0, mobility=0, king_safety=0)
