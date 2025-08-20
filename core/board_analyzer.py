208b176e2654273975d1eb5356c6704f4771e77d
fitil-jpg <appleproapplemac@gmail.com>
2025-08-20 16:23:30 +0300

    Optimize board analyzer by reusing single chess board

--- core/board_analyzer.py
+++ core/board_analyzer.py
@@ -1,11 +1,15 @@
+from .piece import build_chess_board
+
+
 class BoardAnalyzer:
     def __init__(self, board):
         self.board = board
 
     def get_all_attacks(self, color):
+        chess_board = build_chess_board(self.board)
         attacks = set()
         for piece in self.board.get_pieces(color):
-            squares = piece.get_attacked_squares(self.board)
+            squares = piece.get_attacked_squares(self.board, chess_board)
             attacks.update(squares)
         return attacks
 
@@ -17,8 +21,11 @@ class BoardAnalyzer:
     
     def get_defense_map(self):
         """Return mapping of color to defended squares on the board."""
+        chess_board = build_chess_board(self.board)
         defense_map = {'white': set(), 'black': set()}
         for color in ('white', 'black'):
             for piece in self.board.get_pieces(color):
-                defense_map[color].update(piece.get_defended_squares(self.board))
+                defense_map[color].update(
+                    piece.get_defended_squares(self.board, chess_board)
+                )
         return defense_map
