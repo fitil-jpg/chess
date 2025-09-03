1204a42934d202550937efcb61f413fafa8929f3
fitil-jpg <appleproapplemac@gmail.com>
2025-09-04 00:14:32 +0300

    Use caplog for debug assertions in bot tests

--- tests/test_aggressive_bot.py
+++ tests/test_aggressive_bot.py
@@ -1,20 +1,21 @@
+import logging
+
 import chess
 from chess_ai.aggressive_bot import AggressiveBot
 from utils import GameContext
 
 
-def test_capture_gain_scaled_when_behind(capfd, evaluator):
+def test_capture_gain_scaled_when_behind(caplog, evaluator):
     board = chess.Board("8/8/8/3r4/2P5/8/8/8 w - - 0 1")
     evaluator.board = board
     ctx = GameContext(material_diff=-1)
     bot = AggressiveBot(chess.WHITE, capture_gain_factor=1.5)
-    move, score = bot.choose_move(board, context=ctx, evaluator=evaluator, debug=True)
+    with caplog.at_level(logging.DEBUG):
+        move, score = bot.choose_move(board, context=ctx, evaluator=evaluator, debug=True)
     assert move == chess.Move.from_uci("c4d5")
 
     tmp = board.copy(stack=False)
     tmp.push(chess.Move.from_uci("c4d5"))
     expected = 6 + evaluator.position_score(tmp, chess.WHITE)
     assert score == expected
-
-    output = capfd.readouterr().out
-    assert "material deficit" in output
+    assert "material deficit" in caplog.text
