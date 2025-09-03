ea01df28a89a1198c7217174532bd552c32b57a1
fitil-jpg <appleproapplemac@gmail.com>
2025-09-04 01:13:38 +0300

    test: block qd4 to favour knight capture

--- tests/test_criticality.py
+++ tests/test_criticality.py
@@ -5,7 +5,9 @@ from chess_ai.dynamic_bot import DynamicBot
 
 
 def test_knight_two_move_fork_detected_and_neutralised():
-    board = chess.Board("6k1/8/8/6n1/8/5N2/4K3/3Q4 w - - 0 1")
+    # Add a pawn on d2 to block Qd4 and ensure capturing the g5 knight
+    # remains the bot's optimal neutralising move.
+    board = chess.Board("6k1/8/8/6n1/8/5N2/3PK3/3Q4 w - - 0 1")
     evaluator = Evaluator(board)
     critical = evaluator.criticality(board, chess.WHITE)
     assert critical and critical[0][0] == chess.G5
